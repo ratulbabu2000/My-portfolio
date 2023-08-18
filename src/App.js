297226927543e1fd8a4a005dcc58b15da7267bca
ratul <ratulbabu2000@gmail.com>
2023-08-18 11:48:58 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+      <div className="header">
+      <div className="left">
+        <div className="logo">Logo</div>
+      </div>
+      <div className="right">
+        <div className="box">
+          <ul>
+            <li>Home</li>
+            <li>Contect</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <button>Get in</button>
+      </div>
+      </div>
     </div>
   );
 }
